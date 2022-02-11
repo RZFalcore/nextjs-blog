96429bdc1b74896b7aad0e4f171cbb9a8f03838c
Eugene Roy <box.rz.box@gmail.com>
2022-02-11 18:16:43 +0200

    added layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,27 +1,19 @@
 import Head from "next/head";
 import Link from "next/link";
-import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First post</title>
       </Head>
-      <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyOnload"
-        onLoad={() => {
-          console.log(`script loaded correctly, window.FB has been populated`);
-          console.log(window.FB);
-        }}
-      />
       <h1>First post</h1>
       <h2>
         <Link href="/">
           <a>Back to home page</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
