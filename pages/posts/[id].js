ca22443fee156c46688610e656c1f22c8ad4c6f5
Eugene Roy <box.rz.box@gmail.com>
2022-02-16 14:46:48 +0200

    refac single post page to be dynamic

---
+++ pages/posts/[id].js
@@ -0,0 +1,13 @@
+import Layout from "../../components/layout";
+import { getAllPostsIds } from "../../lib/posts";
+
+export async function getStaticPaths() {
+  const paths = getAllPostsIds();
+  return { paths, fallback: false };
+}
+
+export default function Post() {
+  return <Layout></Layout>;
+}
+
+export async function getStaticProps({ params }) {}
