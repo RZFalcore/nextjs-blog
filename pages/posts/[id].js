da56bbe0d7224a547816af2860ad8ac0725a3f98
Eugene Roy <box.rz.box@gmail.com>
2022-02-16 15:44:48 +0200

    polishing post page with styles, added Date comp

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,25 +1,32 @@
+import Head from "next/head";
+import Date from "../../components/date";
 import Layout from "../../components/layout";
 import { getAllPostsIds, getPostData } from "../../lib/posts";
-
+import utilStyles from "../../styles/utils.module.css";
 export async function getStaticPaths() {
   const paths = getAllPostsIds();
   return { paths, fallback: false };
 }
 
 export default function Post({ pageData }) {
-  const { id, title, date } = pageData;
+  const { id, title, date, contentHtml } = pageData;
   return (
     <Layout>
-      {title}
-      <br />
-      {id}
-      <br />
-      {date}
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+      </article>
     </Layout>
   );
 }
 
 export async function getStaticProps({ params }) {
-  const pageData = getPostData(params.id);
+  const pageData = await getPostData(params.id);
   return { props: { pageData } };
 }
